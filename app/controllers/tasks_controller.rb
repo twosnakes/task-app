c6f78f7dc851b43bd202a5b4254c16f70fe036dd
Zoe Lembeck <zlembeck@gmail.com>
2018-04-01 23:04:08 -0500

    working on show, edit, update and destroy tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -11,12 +16,36 @@ class TasksController < ApplicationController
     @task = Task.new(params.require(:task).permit(:title, :description, :deadline))
     if @task.save
       flash[:success] = "Task Added To Your List"
-      redirect_to "/"
+      redirect_to '/'
     else
       flash[:warning] = "Something Went Wrong, Please Try Again!"
-      render "new"
+      render 'new'
     end
   end
 
+  def edit
+  end
 
+  def update
+    if @task.update(task_params)
+    redirect_to tasks_path(@task)
+    else
+    render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to '/'
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])
+    end
 end
